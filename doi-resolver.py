f1e22cbf9a62672666d4f32050d6e5d529b08403
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2018-03-24 18:42:39 -0700

    update code and add state_of_oa dois

--- doi-resolver.py
+++ doi-resolver.py
@@ -1,76 +1,99 @@
 # coding: utf-8
+import argparse
 import datetime
-import requests
-import pandas as pd
+from concurrent.futures import ProcessPoolExecutor
 
+import pandas as pd
+import requests
+from requests_futures.sessions import FuturesSession
 from tqdm import tqdm
 
-from concurrent.futures import ProcessPoolExecutor
-from requests_futures.sessions import FuturesSession
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Mass Construct URLs from DOIs and Resolve')
+    parser.add_argument('-i', '--input', required=True,
+                        help='Input file')
+    parser.add_argument('-c', '--column', default="doi",
+                        help='Define target column')
+    parser.add_argument('-f', '--format', default='https://doi.org/{}',
+                        help='Format string for IDs.\
+                              Example: \'https://doi.org/{}\'')
+    parser.add_argument('-U', '--url', dest='url', action='store_true',
+                        help='Disables URL construction')
+    parser.add_argument('-w', '--workers', default=100, type=int,
+                        help='Number of concurrent workers')
+    parser.add_argument('-b', '--batchsize', default=100, type=int,
+                        help='Batchsize (max memory consumption)')
+    parser.add_argument('-t', '--timeout', default=5, type=int,
+                        help='Timeout for requests')
+    args = vars(parser.parse_args())
 
-# Read input file
-df = pd.read_csv("wos_100k.csv")
-dois = df.doi.tolist()
+    # Options
+    batchsize = args['batchsize']
+    max_workers = args['workers']
+    timeout = args['timeout']
+    col = args['column']
 
-# Options
-batchsize = 100
-max_workers = 100
-timeout = 5
+    # Read input file
+    df = pd.read_csv(args['input'], )
+    targets = df[col].tolist()
 
-# Init output df
-resolved_dois = pd.DataFrame({'doi': dois,
-                              'url': None,
-                              'ts': None,
-                              'err': None,
-                              'err_msg': None,
-                              'status_code': None})
-resolved_dois = resolved_dois.set_index("doi")
+    resolved = pd.DataFrame({col: targets,
+                             'resolved': None,
+                             'ts': None,
+                             'err': None,
+                             'err_msg': None,
+                             'status_code': None})
+    resolved = resolved.set_index(col)
 
-# Split dois into batches
-batches = range(0, len(dois), batchsize)
+    # Split dois into batches
+    batches = range(0, len(targets), batchsize)
 
-# FutureSession
-session = FuturesSession(max_workers=max_workers)
-for i in tqdm(batches, total=len(batches)):
-    futures = []
-    batch = dois[i:i + batchsize]
+    # FutureSession
+    session = FuturesSession(max_workers=max_workers)
+    for i in tqdm(batches, total=len(batches)):
+        futures = []
+        batch = targets[i:i + batchsize]
 
-    # create futures in parallel
-    for doi in batch:
-        now = datetime.datetime.now()
-        future = session.get('https://doi.org/{}'.format(doi),
-                             allow_redirects=True,
-                             timeout=timeout)
+        # create futures in parallel
+        for target in batch:
+            now = datetime.datetime.now()
+            if not args['url']:
+                url = args['format'].format(target)
+            else:
+                url = target
 
-        futures.append({
-            "doi": doi,
-            "ts": str(now),
-            "future": future
-        })
+            future = session.get(url, allow_redirects=True, timeout=timeout)
 
-    # collect future respones and populate df
-    for response in futures:
-        resolved_dois.loc[response['doi'], 'ts'] = response['ts']
-        err = None
-        err_msg = None
-        status = None
+            futures.append({
+                "target": target,
+                "ts": str(now),
+                "future": future
+            })
 
-        try:
-            resolved_dois.loc[response['doi'],
-                              'url'] = response['future'].result().url
-            resolved_dois.loc[response['doi'],
-                              'status_code'] = response['future'].result().status_code
-        except requests.exceptions.Timeout as ex:
-            err_msg = str(ex)
-            err = "Timeout"
-        except requests.exceptions.TooManyRedirects as ex:
-            err_msg = str(ex)
-            err = "TooManyRedirects"
-        except requests.exceptions.RequestException as ex:
-            err_msg = str(ex)
-            err = "RequestException"
+        # collect future respones and populate df
+        for response in futures:
+            err = None
+            err_msg = None
+            status = None
 
-        resolved_dois.loc[response['doi'], 'err'] = err
-        resolved_dois.loc[response['doi'], 'err_msg'] = err_msg
+            ix = response['target']
 
-resolved_dois.to_csv("wos_resolved.csv")
+            try:
+                resolved.loc[ix, 'resolved'] = response['future'].result().url
+                resolved.loc[ix, 'status_code'] = response['future'].result().status_code
+            except requests.exceptions.Timeout as ex:
+                err_msg = str(ex)
+                err = "Timeout"
+            except requests.exceptions.TooManyRedirects as ex:
+                err_msg = str(ex)
+                err = "TooManyRedirects"
+            except requests.exceptions.RequestException as ex:
+                err_msg = str(ex)
+                err = "RequestException"
+
+            resolved.loc[ix, 'err'] = err
+            resolved.loc[ix, 'err_msg'] = err_msg
+            resolved.loc[ix, 'ts'] = response['ts']
+
+    resolved.to_csv(args['input'].split(".csv")[0] + "_resolved.csv")
